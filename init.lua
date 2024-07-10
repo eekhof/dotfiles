fcb9f06089ad5c6e698122f273160c9bbceb0964
Lucas Eekhof <105216949+eekhof@users.noreply.github.com>
2024-07-10 15:52:47 +0200

    add visual mode escape jk rolling shortcut

--- init.lua
+++ init.lua
@@ -131,6 +131,7 @@ end
 vim.g.mapleader = ' '                           -- Map leader key to space
 vim.g.maplocalleader = ' '                      -- Make local leader key same as leader key, required by vimtex, see https://stackoverflow.com/questions/26837425/vim-how-to-use-the-control-key-as-my-local-leader
 imap('jk', '<Esc>')                             -- Map jk to escape from insert mode to prevent reaching for escape - If needing to type literal jk, one has to wait one to two seconds between presses, see https://vim.fandom.com/wiki/Avoid_the_escape_key
+vmap('jk', '<Esc>')                             -- Map jk to escape from visual mode
 
 -- Map putting in visual mode so that text stays selected, but not if using capital P . Capital P is normally used to put text in visual mode, and not yank it to register, because small p yanks overwritten text to register
 vmap('p', 'Pgv')
