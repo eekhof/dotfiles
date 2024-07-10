407e4f6d5829fa1ee543ca37c40ff968233ebc4f
Lucas Eekhof <105216949+eekhof@users.noreply.github.com>
2024-07-10 15:51:51 +0200

    add treesitter

--- init.lua
+++ init.lua
@@ -436,6 +436,8 @@ local plugins = {
     'saadparwaiz1/cmp_luasnip',
     'hrsh7th/nvim-cmp', -- Completions, but needs setup to work for each specific language
     'hrsh7th/cmp-nvim-lsp', -- For this and the following three plugins see recommended config on https://github.com/hrsh7th/nvim-cmp
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-treesitter/nvim-treesitter-textobjects',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
     'petertriho/cmp-git',
@@ -688,6 +690,115 @@ require("catppuccin").setup {
 vim.cmd.colorscheme('catppuccin')
 vim.cmd("AirlineTheme catppuccin") -- Set airline theme to catppuccin
 
+
+-- Treesitter:
+-- require'nvim-treesitter.configs'.setup {
+--     textobjects = { enable = true,
+--         select = {
+--             enable = true,
+--             lookahead = true,
+--             keymaps = {
+--                 -- You can use the capture groups defined in textobjects.scm
+--                 ['af'] = '@function.outer',
+--                 ['if'] = '@function.inner',
+--                 ['ac'] = '@class.outer',
+--                 ['ic'] = '@class.inner',
+--             },
+--         },
+--         move = {
+--             enable = true,
+--             set_jumps = true, -- whether to set jumps in the jumplist
+--             goto_next_start = { [']m'] = '@function.outer', [']]'] = '@class.outer' },
+--             goto_next_end = { [']M'] = '@function.outer', [']['] = '@class.outer' },
+--             goto_previous_start = { ['[m'] = '@function.outer', ['[['] = '@class.outer' },
+--             goto_previous_end = { ['[M'] = '@function.outer', ['[]'] = '@class.outer' },
+--         },
+--         swap = {
+--             enable = true,
+--             swap_next = { ['<leader>>'] = '@parameter.inner' },
+--             swap_previous = { ['<leader><'] = '@parameter.outer' },
+--         },
+--         lsp_interop = {
+--             enable = true,
+--             peek_definition_code = {
+--                 ['gD'] = '@function.outer',
+--             },
+--         },
+--     },
+-- }
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+      ensure_installed = { "c", "python", "lua" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  -- List of parsers to ignore installing (or "all")
+  ignore_install = { "javascript" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    -- disable = { "c", "rust" },
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+    textobjects = { enable = true,
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner',
+                ['ac'] = '@class.outer',
+                ['ic'] = '@class.inner',
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = { [']m'] = '@function.outer', [']]'] = '@class.outer' },
+            goto_next_end = { [']M'] = '@function.outer', [']['] = '@class.outer' },
+            goto_previous_start = { ['[m'] = '@function.outer', ['[['] = '@class.outer' },
+            goto_previous_end = { ['[M'] = '@function.outer', ['[]'] = '@class.outer' },
+        },
+        swap = {
+            enable = true,
+            swap_next = { ['<leader>>'] = '@parameter.inner' },
+            swap_previous = { ['<leader><'] = '@parameter.outer' },
+        },
+        lsp_interop = {
+            enable = true,
+            peek_definition_code = {
+                ['gD'] = '@function.outer',
+            },
+        },
+    },
+}
 -- Templates -----------------------------
 -- augroup templates
 -- When buffer is git commit message edit buffer, jump into insert mode:
