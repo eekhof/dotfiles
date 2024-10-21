8b0f594d94ebade7f0e7126cc27ebbd01dc4de3b
Lucas Eekhof <105216949+eekhof@users.noreply.github.com>
2024-10-21 14:31:56 +0200

    fix copilot enabling for markdown

--- init.lua
+++ init.lua
@@ -531,6 +531,17 @@ require("lazy").setup({ plugins, opts }) -- Start lazy.nvim
 require('copilot').setup({
     suggestion = { enabled = false },
     panel = { enabled = false },
+    filetypes = {
+          yaml = true,
+          markdown = true,
+          help = true,
+          gitcommit = true,
+          gitrebase = true,
+          hgcommit = true,
+          svn = true,
+          cvs = false,
+          ["."] = true,
+    },
 })
 require('gitsigns').setup({ -- Setup gitsigns plugin, see Source: https://github.com/lewis6991/gitsigns.nvim
     preview_config = {
