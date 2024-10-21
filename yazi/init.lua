f727d52e6de3dde6375c659b3f90b62bb0874558
Lucas Eekhof <105216949+eekhof@users.noreply.github.com>
2024-10-21 14:28:55 +0200

    modify sorting and date display

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,3 +1,20 @@
 require("zoxide"):setup {
 	update_db = true,
 }
+
+-- Display the file size and modification time in the file line, see https://yazi-rs.github.io/docs/configuration/yazi/#manager.linemode
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.modified or 0)
+	if time == 0 then
+		time = ""
+    elseif os.date("%d.%m.%Y", time) == os.date("%d.%m.%Y") then
+        time = os.date("           %H:%M", time)
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("     %d.%m %H:%M", time)
+	else
+		time = os.date("%Y %d.%m %H:%M", time)
+	end
+
+	local size = self._file:size()
+	return ui.Line(string.format("%s %s", size and ya.readable_size(size) or "-", time))
+end
