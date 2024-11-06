44acd92cba0e614e6b41634977ad1f7ee5242956
Lucas Eekhof <105216949+eekhof@users.noreply.github.com>
2024-11-06 09:55:21 +0100

    ensure fortan treesitter installation

--- init.lua
+++ init.lua
@@ -804,7 +804,7 @@ vim.cmd("AirlineTheme catppuccin") -- Set airline theme to catppuccin
 -- }
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "python", "lua" },
+    ensure_installed = { "c", "python", "lua", "fortran" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
