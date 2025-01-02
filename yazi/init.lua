fa219b6999faafdcb010055c2771d62069459301
Lucas Eekhof <105216949+eekhof@users.noreply.github.com>
2025-01-02 19:54:38 +0100

    add plugins; fix variable renaming in new version

--- yazi/init.lua
+++ yazi/init.lua
@@ -3,9 +3,30 @@ require("zoxide"):setup {
 	update_db = true,
 }
 
+-- TODO: The restore plugin needs some fixes in the confirmation dialog, and is likely going to be revamped soon, see github for update, load accordingly
+require("restore"):setup({
+    -- Set the position for confirm and overwrite dialogs.
+    -- don't forget to set height: `h = xx`
+    -- https://yazi-rs.github.io/docs/plugins/utils/#ya.input
+    position = { "center", w = 70, h = 40 },
+
+    -- Show confirm dialog before restore.
+    -- NOTE: even if set this to false, overwrite dialog still pop up
+    show_confirm = true,
+
+    -- colors for confirm and overwrite dialogs
+    theme = {
+      title = "blue",
+      header = "green",
+      -- header color for overwrite dialog
+      header_warning = "yellow",
+      list_item = { odd = "blue", even = "blue" },
+    },
+})
+
 -- Display the file size and modification time in the file line, see https://yazi-rs.github.io/docs/configuration/yazi/#manager.linemode
 function Linemode:size_and_mtime()
-	local time = math.floor(self._file.cha.modified or 0)
+	local time = math.floor(self._file.cha.mtime or 0)
 	if time == 0 then
 		time = ""
     elseif os.date("%d.%m.%Y", time) == os.date("%d.%m.%Y") then
--- yazi/plugins/folder-rules.yazi/init.lua
+++ yazi/plugins/folder-rules.yazi/init.lua
@@ -1,11 +1,15 @@
 -- This custom plugin defines folder specific rules
 -- For the disabling of the preloader and previewer in **/mnt/** directory see [[plugin.prepend_preloaders]] and [[plugin.prepend_previewers]] entries in yazi.toml
+-- TODO: Add overwrite for delete inside sshfs-mounted folders. Deletion does not work there, and it is likely that this is because the trashing function that is normally bound to the delete key does not work with servers - perhaps remap it to manual mv command to serverside .trash folder
 local function setup()
     ps.sub("cd", function()
 	-- Sort Downloads folder chronologically instead of alphabetically:
-        if cwd:ends_with("Downloads") then
-            ya.manager_emit("sort", { "modified", reverse = true, dir_first = false })
-        end
+	local cwd = cx.active.current.cwd
+	if cwd:ends_with("Downloads") then
+		ya.manager_emit("sort", { "mtime", reverse = true, dir_first = false })
+	else
+		ya.manager_emit("sort", { "natural", reverse = false, dir_first = true })
+	end
     end)
 end
 
